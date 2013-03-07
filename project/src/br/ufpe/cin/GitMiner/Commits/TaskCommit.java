d26bc78ef72eec949c8254bfa6785eb1df19f5f2
prga <prga@cin.ufpe.br>
2013-03-07 16:24:42 -0300

    correcting loadFilesFromAddedDirectories method

---
+++ project/src/br/ufpe/cin/GitMiner/Commits/TaskCommit.java
@@ -0,0 +1,21 @@
+package br.ufpe.cin.GitMiner.Commits;
+
+
+
+public class TaskCommit extends BaseCommit {
+	
+	
+	
+public TaskCommit(String name, String url, String sha){
+		
+		super(name, url, sha);
+		
+		
+	}
+
+
+
+
+
+
+}
