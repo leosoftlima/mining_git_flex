91d14d6fdc149bff2f4540c0b47233eb04177fcb
Thaís <tabr@cin.ufpe.br>
2015-09-25 14:03:12 -0300

    Refactoring.

--- src/search/Main.java
+++ src/search/Main.java
@@ -1,106 +1,22 @@
 package search;
 
-import au.com.bytecode.opencsv.CSVWriter;
-import util.Util;
-import java.io.FileWriter;
-import java.util.ArrayList;
-
 
 public class Main {
 
-    private static int counter = 0;
-    private static int selectedCounter = 0;
-
-    private static void resetCounters(){
-        counter = 0;
-        selectedCounter = 0;
-    }
-
-    private static void check(Repository repository, CSVWriter writer, int index) {
-        try {
-            repository.downloadZip();
-            repository.unzip();
-            if (!repository.hasFeatureFile()) {
-                System.out.println("The project does not contain feature file!");
-                repository.deleteAll();
-            } else{
-                System.out.println("The project does contain feature file!");
-                selectedCounter++;
-                repository.deleteUnzipedDir();
-                writer.writeNext(new String[]{String.valueOf(index), repository.getUrl()});
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private static void check(Repository repository) {
-        try {
-            repository.downloadZip();
-            repository.unzip();
-            if (!repository.hasFeatureFile()) {
-                System.out.println("The project does not contain feature file!");
-                repository.deleteAll();
-            } else{
-                System.out.println("The project does contain feature file!");
-                selectedCounter++;
-                repository.deleteUnzipedDir();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void checkProject(String url){
-        Repository repo = new Repository(url);
-        check(repo);
-    }
-
-    public static void checkProject(String url, String branch){
-        Repository repo = new Repository(url, branch);
-        check(repo);
-    }
-
-    public static void searchCucumberProjectsDefaultBranch(){
-        ArrayList<Repository> repositories;
-        resetCounters();
-
-        try {
-            repositories = FileHandler.extractProjectsDefaultBranch();
-            for(Repository repository: repositories){
-                check(repository);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void searchCucumberProjects(){
-        CSVWriter writer;
-        ArrayList<Repository> repositories;
-        resetCounters();
-
-        try {
-            repositories = FileHandler.extractProjects();
-            writer = new CSVWriter(new FileWriter(Util.SELECTED_PROJECTS_FILE));
-            writer.writeNext(new String[]{"index", "repository_url"});
-            for(int i=0; i<repositories.size(); i++){
-                check(repositories.get(i), writer, i+2);
-            }
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     public static void main(String[] args){
+        SearchManager searcher = new SearchManager();
+
         //Downloading and unzipping a project (example)
-        checkProject("https://github.com/spgroup/rgms/archive/master.zip");
+        searcher.checkProject("https://github.com/spgroup/rgms/archive/master.zip");
 
         // Downloading and unzipping projects from csv file
-        searchCucumberProjects();
-        System.out.printf("Number of analyzed projects: %d%n", counter);
-        System.out.printf("Number of selected projects: %d (%.2f%%)%n", selectedCounter, ((double)selectedCounter/counter)*100);
+        searcher.searchCucumberProjects();
+
+        System.out.printf("Number of analyzed projects: %d%n", searcher.getCounter());
+        System.out.printf("Number of selected projects: %d (%.2f%%)%n", searcher.getSelectedCounter(),
+                ((double)searcher.getSelectedCounter()/searcher.getCounter())*100);
+
+        searcher.listSelectedRepositories();
     }
 
 }
---
+++ src/search/SearchManager.java
@@ -0,0 +1,130 @@
+package search;
+
+
+import au.com.bytecode.opencsv.CSVWriter;
+import util.Util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SearchManager {
+
+    private int counter;
+    private ArrayList<Repository> selected;
+
+    public SearchManager(){
+        selected = new ArrayList<>();
+    }
+
+    private void resetCounters(){
+        counter = 0;
+        selected.clear();
+    }
+
+    private void check(Repository repository, CSVWriter writer, int index) {
+        try {
+            counter++;
+            repository.downloadZip();
+            repository.unzip();
+            if (!repository.hasFeatureFile()) {
+                System.out.println("The project does not contain feature file!");
+                repository.deleteAll();
+            } else{
+                System.out.println("The project does contain feature file!");
+                selected.add(repository);
+                repository.deleteUnzipedDir();
+                writer.writeNext(new String[]{String.valueOf(index), repository.getUrl()});
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void check(Repository repository) {
+        try {
+            counter++;
+            repository.downloadZip();
+            repository.unzip();
+            if (!repository.hasFeatureFile()) {
+                System.out.println("The project does not contain feature file!");
+                repository.deleteAll();
+            } else{
+                System.out.println("The project does contain feature file!");
+                selected.add(repository);
+                repository.deleteUnzipedDir();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /* The input is the zip file url. */
+    public void checkProject(String url){
+        Repository repo = new Repository(url);
+        check(repo);
+    }
+
+    public void checkProjectFromUrlFile(String file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(file));
+        while (scanner.hasNextLine()){
+            String url = scanner.nextLine();
+            System.out.println(url);
+            checkProject(url);
+        }
+    }
+
+    public void checkProject(String url, String branch){
+        Repository repo = new Repository(url, branch);
+        check(repo);
+    }
+
+    public void searchCucumberProjectsDefaultBranch(){
+        ArrayList<Repository> repositories;
+        resetCounters();
+
+        try {
+            repositories = FileHandler.extractProjectsDefaultBranch();
+            for(Repository repository: repositories){
+                check(repository);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void searchCucumberProjects(){
+        CSVWriter writer;
+        ArrayList<Repository> repositories;
+        resetCounters();
+
+        try {
+            repositories = FileHandler.extractProjects();
+            writer = new CSVWriter(new FileWriter(Util.SELECTED_PROJECTS_FILE));
+            writer.writeNext(new String[]{"index", "repository_url"});
+            for(int i=0; i<repositories.size(); i++){
+                check(repositories.get(i), writer, i+2);
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getCounter(){
+        return counter;
+    }
+
+    public int getSelectedCounter(){
+        return selected.size();
+    }
+
+    public void listSelectedRepositories(){
+        for(Repository r: selected){
+            System.out.printf("url: %s, branch: %s, zip: %s%n", r.getUrl(), r.getBranch(), r.getZipfileName());
+        }
+    }
+
+}
