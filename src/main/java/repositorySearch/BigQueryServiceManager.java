5fb07f3e5c054f52a8f0c53b5b3f22c725de4338
Thaís <tabr@cin.ufpe.br>
2015-10-22 17:58:47 -0200

    Bugfix.

--- src/main/java/repositorySearch/BigQueryServiceManager.java
+++ src/main/java/repositorySearch/BigQueryServiceManager.java
@@ -111,29 +111,19 @@ public class BigQueryServiceManager {
         Bigquery bigquery = createAuthorizedClient();
 
         // Configure the query
-        if(query==null || query.isEmpty()) {
-            query = "SELECT repository_url, repository_master_branch, payload_commit_msg, "
-                    + "(repository_url + '/commit/' + payload_commit_id) AS commit_link, payload_commit_id, created_at, repository_watchers "
-                    + "FROM [githubarchive:github.timeline] "
-                    + "WHERE PARSE_UTC_USEC(repository_created_at) <= PARSE_UTC_USEC ('2015-08-31 23:59:59') "
-                    + "AND PARSE_UTC_USEC(repository_created_at) >= PARSE_UTC_USEC ('2010-08-31 23:59:59') "
-                    + "AND type = 'PushEvent' "
-                    + "AND repository_language = 'Groovy' "
-                    + "AND ( (LOWER(payload_commit_msg) LIKE '[#%]%') "
-                    + "OR (LOWER(payload_commit_msg) LIKE '[fix% #%]%') "
-                    + "OR (LOWER(payload_commit_msg) LIKE '[complet% #%]%') "
-                    + "OR (LOWER(payload_commit_msg) LIKE '[finish% #%]%') ) "
-                    + "GROUP BY repository_url, repository_master_branch, payload_commit_msg, commit_link, payload_commit_id, created_at, repository_watchers "
-                    + "ORDER BY repository_url";
+        if(query!=null && !query.isEmpty()) {
+            // Run query
+            List<TableRow> rows = executeQuery(query, bigquery, projectId);
+            if(rows != null){
+                System.out.println("Result size: "+rows.size());
+
+                // Save result
+                exportResults(rows);
+                System.out.printf("The search result is saved at %s%n", Util.PROJECTS_FILE);
+            }
+            else
+                System.out.println("Result size: 0");
         }
-
-        // Run query
-        List<TableRow> rows = executeQuery(query, bigquery, projectId);
-        System.out.println("Result size: "+rows.size());
-
-        // Save result
-        exportResults(rows);
-        System.out.printf("The search result is saved at %s%n", Util.PROJECTS_FILE);
     }
 
 }
